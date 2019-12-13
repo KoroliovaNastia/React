8b19345654decdfe3b9235172a6ce1f1425c880e
Anastasiya_Karaliova <Anastasiya_Karaliova@epam.com>
2019-12-13 16:28:43 +0300

    redux-persist

--- SimpleReactApp/src/redux/store.js
+++ SimpleReactApp/src/redux/store.js
@@ -1,11 +1,22 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers/index";
-//import swaggerClient from 'redux-swagger-client'
+import {persistReducer, persistStore} from 'redux-persist'
 import thunk from 'redux-thunk'
+import { AsyncResource } from "async_hooks";
+
+
+const persistConfig = {
+  key:'movieReducer',
+  storage: storage,
+  whitelist: ['movieReducer']
+}
+
+const pReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
-      rootReducer,
-      applyMiddleware(thunk)
-  );
+    pReducer,
+    applyMiddleware(thunk, autoRehydrate)
+);
 
-  export default store
+const persistor = persistStore(store/*, { storage: AsyncResource}*/);
+export default {persistor,  store}
